5614fd240d18510969033168c85465342647b78d
alamgir-mondal <97507836+alamgir-mondal@users.noreply.github.com>
2022-08-08 12:05:53 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activity6Runner.java
@@ -0,0 +1,11 @@
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "Features",
+    glue = {"stepDefinitions"},
+    tags = "@SimpleAlert",
+    plugin = {"html: test-reports"},
+    monochrome = true
+)
+
+public class ActivitiesRunner {
+}
